7823d14d9fb956aa0d7f8d8114dfb02f0cd0e101
sreechith <sreechith.hidragen@gmail.com>
2024-06-12 10:10:39 +0530

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
-const app =express();
+const app = express();
 
 const n = "s";
-app.listen(process.env.PORT, () =>
-{
-
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
